b77be5a39e3364afc19cdec6f0e21ca8ab4e4a2a
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-08-30 20:04:35 -0400

    Create pytest.py
    test cases

---
+++ pytest.py
@@ -0,0 +1,52 @@
+import os.path
+import sys
+from PP1 import *
+from tud_tests import *
+
+def test_q1(capsys):
+
+  try:
+    exists = os.path.exists("PP1.py")
+    assert exists
+  except:
+    sys.exit()
+
+  q1()
+  capture = capsys.readouterr()
+  assert captured.out = "Hello World\n"
+
+def test_q2(capsys):
+
+  try:
+    exists = os.path.exists("PP1.py")
+    assert exists
+  except:
+    sys.exit()
+
+  q2()
+  capture = capsys.readouterr()
+  assert captured.out = "1\n2\n3\n4\n5\n"
+
+def test_q3(capsys):
+
+  try:
+    exists = os.path.exists("PP1.py")
+    assert exists
+  except:
+    sys.exit()
+
+  q3()
+  capture = capsys.readouterr()
+  assert captured.out = " Help Me!\n"
+
+def test_q4(capsys):
+
+  try:
+    exists = os.path.exists("PP1.py")
+    assert exists
+  except:
+    sys.exit()
+
+  q4()
+  capture = capsys.readouterr()
+  assert captured.out = "3 x 3 = 9n"
